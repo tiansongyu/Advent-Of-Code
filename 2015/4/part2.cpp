c8016cab00776138f0fb0569df1b82d0e007184f
invoker__qq <478880214@qq.com>
2021-09-17 10:28:04 +0800

    Add Day 4 Solution

---
+++ 2015/4/part2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include "md5.h"
+#include <string>
+int main()
+{
+	int i = 0;
+	while (true)
+	{
+		std::string str = "yzbqklnj";
+		str = str + std::to_string(i);
+		str = md5::md5_hash_hex(str);
+		bool flag = true;
+		for (int j = 0; j < 6; j++)
+		{
+			if (str[j] != '0')
+				flag = false;
+		}
+		if (flag)
+		{
+			std::cout << str << std::endl;
+			std::cout << i << std::endl;
+			break;
+		}
+		i++;
+	}
+	return 0;
+}
