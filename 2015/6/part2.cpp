aa68b8b3d2599b16ac776cc24a41d4c530676f59
tiansongyu <478880214@qq.com>
2021-09-22 10:17:07 +0800

    Add Day 6 Solution

---
+++ 2015/6/part2.cpp
@@ -0,0 +1,77 @@
+#include <fmt/core.h>
+#include <cmath>
+#include <set>
+#include <fstream>
+#include <strstream>
+#include <istream>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <vector>
+#include <fmt/core.h>
+
+std::vector<int> translation(std::string str, int number)
+{
+    std::vector<int> pos;
+
+    size_t amt = 0, two = 0, three = 0, four = 0;
+
+    pos.emplace_back(std::stoi(str.substr(number), &amt));
+    pos.emplace_back(std::stoi(str.substr(number + 1 + amt), &two));
+    pos.emplace_back(std::stoi(str.substr(number + 1 + amt + 9 + two), &three));
+    pos.emplace_back(std::stoi(str.substr(number + 1 + amt + 9 + two + 1 + three), &four));
+    fmt::print("{} {} {} {} {} \n", pos[0], pos[1], pos[2], pos[3], str);
+    return pos;
+}
+int main()
+{
+    std::ifstream f("./input.txt");
+    if (!f.is_open())
+        return false;
+    int all_sum = 0;
+    int light[1000][1000] = {0};
+    while (!f.eof())
+    {
+        char line[128];
+        f.getline(line, 128);
+        std::strstream s;
+        std::string str;
+        s << line;
+        str = line;
+        char flag;
+        std::vector<int> pos;
+        if (str[5] == 'e')
+        {
+            flag = 'e';
+            pos = translation(str, 7);
+        }
+        if (str[6] == 'f')
+        {
+            flag = 'f';
+            pos = translation(str, 9);
+        }
+        if (str[6] == 'n')
+        {
+            flag = 'n';
+            pos = translation(str, 8);
+        }
+
+        for (int j = pos[1]; j < pos[3] + 1; j++)
+            for (int i = pos[0]; i < pos[2] + 1; i++)
+            {
+                if (flag == 'e')
+                    light[i][j] += 2;
+                if (flag == 'f')
+                    if (light[i][j] != 0)
+                        light[i][j]--;
+                if (flag == 'n')
+                    light[i][j]++;
+            }
+    }
+    for (int i = 0; i < 1000; i++)
+        for (int j = 0; j < 1000; j++)
+            all_sum += light[i][j];
+    fmt::print("{}\n", all_sum);
+    return 0;
+}
